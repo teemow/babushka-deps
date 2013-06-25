55bdc6ea015ac0de7229db803a17897f2901df30
Timo Derstappen <teemow@gmail.com>
2013-06-25 11:52:51 +0000

    added yaourt dependency to template

--- arm-none-eabi-binutils.rb
+++ arm-none-eabi-binutils.rb
@@ -1,4 +1,3 @@
 dep "arm-none-eabi-binutils.yaourt" do
-  requires "yaourt.src"
   provides "arm-none-eabi-ld"
 end
--- templates/yaourt.rb
+++ templates/yaourt.rb
@@ -2,6 +2,7 @@ meta :yaourt do
   accepts_value_for :package, :basename
   accepts_list_for :provides, :basename
   template {
+    requires_when_unmet "yaourt.src"
     met? { in_path?(provides) }
     meet {
       log_shell "Installing #{package}", "sudo yaourt --noconfirm -Sb #{package}"
