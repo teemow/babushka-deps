ad2466632b81ba30acd8a8bea3bdacdb8739b9e9
Timo Derstappen <teemow@gmail.com>
2013-06-25 14:05:37 +0000

    added babushka deps to basic dep

--- basics.rb
+++ basics.rb
@@ -15,6 +15,16 @@ dep "gem-environment" do
   }
 end
 
+dep 'babushka-deps', :github_user, :babushka_deps_repo, :babushka_deps_dir do
+
+  def repo
+    "git@github.com:#{github_user}/#{babushka_deps_repo}.git"
+  end
+
+  met? { File.directory?(babushka_deps_dir) && shell("(cd #{babushka_deps_dir} && git remote -v)")[repo] }
+  meet { shell "git clone #{repo} #{babushka_deps_dir}" }
+end
+ 
 dep "basics" do
-  requires %w{vim.bin whois.bin wget.bin htop.bin iostat.bin gem-environment}
+  requires %w{vim.bin whois.bin wget.bin htop.bin iostat.bin gem-environment}, "babushka-deps".with("teemow", "babushka-deps", "~/.babushka" / "deps")
 end
