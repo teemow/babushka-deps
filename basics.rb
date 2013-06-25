35ad80a2aa0452a18c3872aa0a6043de384a6ad8
Timo Derstappen <teemow@gmail.com>
2013-06-25 10:49:38 +0000

    added gem path for arch to basics

--- basics.rb
+++ basics.rb
@@ -5,7 +5,16 @@ dep "htop.bin"
 dep "iostat.bin" do
   installs "sysstat"
 end
+dep "gem-environment" do
+  met? {
+    shell "grep rubygems ~/.zprofile"
+  }
+  meet {
+    shell "echo 'PATH=$PATH:$(ruby -rubygems -e \"puts Gem.user_dir\")/bin' >> ~/.zprofile"
+    shell "echo 'export GEM_HOME=~/.gem/ruby/2.0.0' >> ~/.zprofile"
+  }
+end
 
 dep "basics" do
-  requires %w{vim.bin whois.bin wget.bin htop.bin iostat.bin}
+  requires %w{vim.bin whois.bin wget.bin htop.bin iostat.bin gem-environment}
 end
