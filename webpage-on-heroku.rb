2772e99c4fb12bf2a3b51a3fb1b2c20799e61e74
Timo Derstappen <teemow@gmail.com>
2013-06-28 21:31:31 +0000

    update steps for heroku angular app

--- webpage-on-heroku.rb
+++ webpage-on-heroku.rb
@@ -5,4 +5,18 @@ dep "compass.gem"
 
 dep "webpage-on-heroku" do
   requires "nodejs", "nodejs-basics", "heroku-toolbelt.yaourt", "generator-angular.npm", "compass.gem", "generator-heroku.npm", "coffee.npm"
+  met? {
+    hostname = shell "hostname"
+    log ""
+    log "Don't forget!"
+    log ""
+    log "$ cd ~/#{hostname}"
+    log "$ yo angular #{hostname} --coffee"
+    log "$ npm install"
+    log "$ bower install"
+    log "$ grunt build"
+    log "$ yo heroku"
+    log ""
+    true
+  }
 end
